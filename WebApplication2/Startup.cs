204dceb23d284b80f4b7e7d64bf947f4b21950d1
karl-neu <karlneumann699@gmail.com>
2020-11-07 12:21:13 -0800

    Add connection string and di

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
 
 namespace WebApplication2
 {
@@ -25,6 +27,9 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<carsDatabaseContext>(opt =>
+               opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
 
